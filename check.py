2b13db84225841118466a0947a947e979f15d5a2
Shubham dubey <shubh@Shubhams-MBP-2.fios-router.home>
2020-04-03 13:02:53 -0400

    first commit

---
+++ check.py
@@ -0,0 +1,6 @@
+# create a Statement that will print out words that start with 's':
+
+st = 'Print only the words that start with s in this sentence'
+for word in st.split():
+    if word[0] == 's':
+        print(word)
